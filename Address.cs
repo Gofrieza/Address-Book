e352a45f06a47ad34e779f213b573cddc2103418
Gofrieza <93874332+Gofrieza@users.noreply.github.com>
2022-02-18 14:56:23 +0530

    Add files via upload

---
+++ Address.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook
+{
+    internal class Address
+    {
+
+        public string name;
+        public string address;
+
+        public Address(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+    }
+}
